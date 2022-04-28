27a4b4c22ee6397355b6434219516c555dc867aa
Maksud Bhaldar <macbhaldar@gmail.com>
2022-04-28 21:14:11 +0530

    Update tensorflow-variables.py

--- tensorflow-variables.py
+++ tensorflow-variables.py
@@ -243,3 +243,177 @@ with tf.device('GPU:0'):
   k = a * b
 
 print(k)
+
+
+# basic math on tensors, including addition, element-wise multiplication, and matrix multiplication.
+a = tf.constant([[1, 2],
+                 [3, 4]])
+b = tf.constant([[1, 1],
+                 [1, 1]]) # Could have also said `tf.ones([2,2])`
+
+print(tf.add(a, b), "\n")
+print(tf.multiply(a, b), "\n")
+print(tf.matmul(a, b), "\n")
+
+print(a + b, "\n") # element-wise addition
+print(a * b, "\n") # element-wise multiplication
+print(a @ b, "\n") # matrix multiplication
+
+# Tensors are used in all kinds of operations
+c = tf.constant([[4.0, 5.0], [10.0, 1.0]])
+
+# Find the largest value
+print(tf.reduce_max(c))
+# Find the index of the largest value
+print(tf.math.argmax(c))
+# Compute the softmax
+print(tf.nn.softmax(c))
+
+# Tensors and tf.TensorShape objects have convenient properties for accessing these:
+rank_4_tensor = tf.zeros([3, 2, 4, 5])
+print("Type of every element:", rank_4_tensor.dtype)
+print("Number of axes:", rank_4_tensor.ndim)
+print("Shape of tensor:", rank_4_tensor.shape)
+print("Elements along axis 0 of tensor:", rank_4_tensor.shape[0])
+print("Elements along the last axis of tensor:", rank_4_tensor.shape[-1])
+print("Total number of elements (3*2*4*5): ", tf.size(rank_4_tensor).numpy())
+
+
+# Indexing
+## Single-axis indexing
+rank_1_tensor = tf.constant([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+print(rank_1_tensor.numpy())
+
+print("First:", rank_1_tensor[0].numpy())
+print("Second:", rank_1_tensor[1].numpy())
+print("Last:", rank_1_tensor[-1].numpy())
+
+# Indexing with a : slice keeps the axis:
+print("Everything:", rank_1_tensor[:].numpy())
+print("Before 4:", rank_1_tensor[:4].numpy())
+print("From 4 to the end:", rank_1_tensor[4:].numpy())
+print("From 2, before 7:", rank_1_tensor[2:7].numpy())
+print("Every other item:", rank_1_tensor[::2].numpy())
+print("Reversed:", rank_1_tensor[::-1].numpy())
+
+# Multi-axis indexing
+print(rank_2_tensor.numpy())
+# Pull out a single value from a 2-rank tensor
+print(rank_2_tensor[1, 1].numpy())
+
+# Get row and column tensors
+print("Second row:", rank_2_tensor[1, :].numpy())
+print("Second column:", rank_2_tensor[:, 1].numpy())
+print("Last row:", rank_2_tensor[-1, :].numpy())
+print("First item in last column:", rank_2_tensor[0, -1].numpy())
+print("Skip the first row:")
+print(rank_2_tensor[1:, :].numpy(), "\n")
+
+print(rank_3_tensor[:, :, 4])
+
+# Shape returns a `TensorShape` object that shows the size along each axis
+x = tf.constant([[1], [2], [3]])
+print(x.shape)
+
+# You can convert this object into a Python list, too
+print(x.shape.as_list())
+
+# You can reshape a tensor to a new shape.
+# Note that you're passing in a list
+reshaped = tf.reshape(x, [1, 3])
+
+print(x.shape)
+print(reshaped.shape)
+
+## The data maintains its layout in memory and a new tensor is created, with the requested shape, pointing to the same data. 
+## TensorFlow uses C-style "row-major" memory ordering, where incrementing the rightmost index corresponds to a single step in memory.
+
+print(rank_3_tensor)
+
+# a `-1` passed in the `shape` argument says "Whatever fits".
+print(tf.reshape(rank_3_tensor, [-1]))
+
+print(tf.reshape(rank_3_tensor, [3*2, 5]), "\n")
+print(tf.reshape(rank_3_tensor, [3, -1]))
+
+
+# Bad examples: don't do this
+
+## You can't reorder axes with reshape.
+print(tf.reshape(rank_3_tensor, [2, 3, 5]), "\n") 
+
+## This is a mess
+print(tf.reshape(rank_3_tensor, [5, 6]), "\n")
+
+## This doesn't work at all
+try:
+  tf.reshape(rank_3_tensor, [7, -1])
+except Exception as e:
+  print(f"{type(e).__name__}: {e}")
+  
+
+# Ragged Tensors
+## A tensor with variable numbers of elements along some axis is called "ragged".
+ragged_list = [
+    [0, 1, 2, 3],
+    [4, 5],
+    [6, 7, 8],
+    [9]]
+try:
+  tensor = tf.constant(ragged_list)
+except Exception as e:
+  print(f"{type(e).__name__}: {e}")
+ragged_tensor = tf.ragged.constant(ragged_list)
+print(ragged_tensor)
+print(ragged_tensor.shape)
+
+
+# String tensors
+## tf.string is a dtype, which is to say you can represent data as strings (variable-length byte arrays) in tensors.
+# Tensors can be strings, too here is a scalar string.
+scalar_string_tensor = tf.constant("Gray wolf")
+print(scalar_string_tensor)
+# If you have three string tensors of different lengths, this is OK.
+tensor_of_strings = tf.constant(["Gray wolf",
+                                 "Quick brown fox",
+                                 "Lazy dog"])
+# Note that the shape is (3,). The string length is not included.
+print(tensor_of_strings)
+
+tf.constant("🥳👍") # If you pass unicode characters they are utf-8 encoded.
+
+# You can use split to split a string into a set of tensors
+print(tf.strings.split(scalar_string_tensor, sep=" "))
+
+# it turns into a `RaggedTensor` if you split up a tensor of strings, as each string might be split into a different number of parts.
+print(tf.strings.split(tensor_of_strings))
+
+# tf.string.to_number:
+text = tf.constant("1 10 100")
+print(tf.strings.to_number(tf.strings.split(text, " ")))
+
+
+byte_strings = tf.strings.bytes_split(tf.constant("Duck"))
+byte_ints = tf.io.decode_raw(tf.constant("Duck"), tf.uint8)
+print("Byte strings:", byte_strings)
+print("Bytes:", byte_ints)
+
+# split it up as unicode and then decode it
+unicode_bytes = tf.constant("アヒル 🦆")
+unicode_char_bytes = tf.strings.unicode_split(unicode_bytes, "UTF-8")
+unicode_values = tf.strings.unicode_decode(unicode_bytes, "UTF-8")
+
+print("\nUnicode bytes:", unicode_bytes)
+print("\nUnicode chars:", unicode_char_bytes)
+print("\nUnicode values:", unicode_values)
+
+
+# Sparse tensors
+## Sparse tensors store values by index in a memory-efficient manner
+sparse_tensor = tf.sparse.SparseTensor(indices=[[0, 0], [1, 2]],
+                                       values=[1, 2],
+                                       dense_shape=[3, 4])
+print(sparse_tensor, "\n")
+
+# You can convert sparse tensors to dense
+print(tf.sparse.to_dense(sparse_tensor))
